b146dfd6141f2f7973a93c61dbb8b42e48dbf2af
kbengine <3603661@qq.com>
2016-11-09 11:39:26 +0800

    up

--- scripts/cell/interfaces/Combat.py
+++ scripts/cell/interfaces/Combat.py
@@ -122,7 +122,7 @@ class Combat(CombatPropertys):
 		virtual method.
 		检查敌人距离
 		"""
-		dist = entity.position.distTo(self.position) <= 30.0
+		dist = entity.position.distTo(self.position)
 		if dist > 30.0:
 			INFO_MSG("%s::checkEnemyDist: %i id=%i, dist=%f." % (self.getScriptName(), self.id, entity.id, dist))
 			return False
