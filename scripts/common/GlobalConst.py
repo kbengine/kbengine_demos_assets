55001d823f2bdd89b1fffb7acb1a2180a58b39d5
kbengine <3603661@qq.com>
2016-11-30 16:03:54 +0800

    up

--- scripts/common/GlobalConst.py
+++ scripts/common/GlobalConst.py
@@ -10,6 +10,7 @@ GC_SKILL_ENTITY_DEAD				= 0x002		# Entity已经死亡
 
 # 不同demo所对应的地图
 g_demoMaps = {
+	b'kbengine_ue4_demo' : 7,
 	b'kbengine_cocos2d_js_demo' : 6,
 	b'kbengine_unity3d_demo' : 3,
 	b'kbengine_ogre_demo' : 2,
