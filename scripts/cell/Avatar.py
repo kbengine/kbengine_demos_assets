e59e8fac68580f6ef0afe6cb69ceb463e5fd0385
kbengine <3603661@qq.com>
2016-12-08 10:32:35 +0800

    up

--- scripts/cell/Avatar.py
+++ scripts/cell/Avatar.py
@@ -37,7 +37,11 @@ class Avatar(KBEngine.Entity,
 		# 设置每秒允许的最快速度, 超速会被拉回去
 		self.topSpeed = self.moveSpeed + 5.0
 		# self.topSpeedY = 10.0
-			
+		
+		# 如果为7说明在UE4地图中，那么为了配合demo的移动速度，我们将限制设置得更大一些
+		if self.spaceUType == 7
+			self.topSpeed = 0
+
 	def isPlayer(self):
 		"""
 		virtual method.
