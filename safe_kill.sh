be099749ba6fec5f459f4a1ac46d52cb47e81450
jay602 <857024709@qq.com>
2019-02-21 15:08:54 +0800

    Linux平台遇到目录有空格的情况下，shell脚本执行有问题 #693

--- safe_kill.sh
+++ safe_kill.sh
@@ -8,12 +8,10 @@ bcontain=`echo $currPath|grep $keyStr|wc -l`
 
 if [ $bcontain = 0 ]
 then
-	export KBE_ROOT=$(cd ../; pwd)
+	export KBE_ROOT="$(cd ../; pwd)"
 else
 	export KBE_ROOT="$(pwd | awk -F "/kbengine/" '{print $1}')/kbengine"
 fi
-
-
 
 export KBE_RES_PATH="$KBE_ROOT/kbe/res/:$(pwd):$(pwd)/res:$(pwd)/scripts/"
 export KBE_BIN_PATH="$KBE_ROOT/kbe/bin/server/"
@@ -23,8 +21,9 @@ XUID=$?
 
 if [ $XUID = 0 ]
 then
-	XUID=`$KBE_BIN_PATH/kbcmd --getuid`
+	KBCMD_PATH="$KBE_BIN_PATH/kbcmd"
+	XUID=`"$KBCMD_PATH" --getuid`
 	echo UID=$XUID
 fi
 
-python $KBE_ROOT/kbe/tools/server/pycluster/cluster_controller.py stop $XUID
+python "$KBE_ROOT/kbe/tools/server/pycluster/cluster_controller.py" stop $XUID
